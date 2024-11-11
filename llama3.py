ee272f269b8c012eedbe2b5294b014d244099745
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2024-11-12 01:02:23 +0900

    Create llama3.py

---
+++ llama3.py
@@ -0,0 +1,48 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+def get_time():
+    return str("   [" + datetime.now().strftime("%H:%M:%S") + "]")
+
+# 모델과 토크나이저 로드
+print("시작"+get_time())
+model_name = "verygood7/llama3-ko-8b"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+print("토크나이저 완료"+get_time())
+model = AutoModelForCausalLM.from_pretrained(model_name)
+print("모델 완료"+get_time())
+
+# 질문 입력
+question = "프랑스의 수도는?"
+
+# 입력 텍스트를 토큰화하고 모델에 입력
+inputs = tokenizer.encode(question, return_tensors="pt")
+print("엔코딩 input 완료"+get_time())
+
+outputs = model.generate(inputs, max_length=50)
+print("생성 output 완료"+get_time())
+
+# 답변 출력
+answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
+print("디코딩 answer 완료"+get_time())
+print(answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
