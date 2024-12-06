e6c215bfb3588bcffcc37cc65b926224096b7f2c
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2024-12-06 10:39:45 +0900

    Update l28.py

--- l28.py
+++ l28.py
@@ -10,8 +10,8 @@ from langchain_llama3 import *
 st.set_page_config(page_title="라마3", page_icon="🦙")
 
 
-# pip install streamlit pypdf faiss-gpu kiwipiepy -U langchain-community transformers torch
-# !pip install pyngrok streamlit pypdf faiss-gpu kiwipiepy -U langchain-community
+# pip install streamlit kiwipiepy -U langchain-community transformers torch
+# !pip install pyngrok streamlit kiwipiepy -U langchain-community
 # !ngrok authtoken 2pfBXJdvp34yPDeEAVXCrvFS2aD_5guh96guiVvtwcnmEsPo9
 
 # from pyngrok import ngrok
