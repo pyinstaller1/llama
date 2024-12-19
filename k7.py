d80ddea5df8c08791e258079030b6afe01e08e1f
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2024-12-19 16:02:56 +0900

    Add files via upload

--- k7.py
+++ k7.py
@@ -5,46 +5,115 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import urllib3
+import warnings
 
 
 
-list = "найду"# "найти"
+http = urllib3.PoolManager(cert_reqs='CERT_NONE', assert_hostname=False)
+warnings.filterwarnings('ignore', category=urllib3.exceptions.InsecureRequestWarning)
+
+
+
+
+list = "стороны    "
+# list = "найду"
+# list = "найти"
 list = re.sub(r'\u0301', '', list)
 print(list)
 
 html = requests.get("https://en.wiktionary.org/wiki/" + list)
 soup = BeautifulSoup(html.text, "html.parser")
-# i_tag = soup.find("i", {"class": "Cyrl mention"})
-div = soup.find("div", {"id": "mw-content-text"})
-# print(div)
 
-
-
-
-# v<span class="form-of-definition use-with-mention">
 span = soup.find("span", {"class": "form-of-definition use-with-mention"})
-print(span)
+
 
 
 if span:
     print("span yes")
     i_tag = soup.find("i", {"class": "Cyrl mention"})
     print(i_tag.text)
+
+    html = requests.get("https://en.wiktionary.org/wiki/" + re.sub(r'\u0301', '', i_tag.text))
+    soup = BeautifulSoup(html.text, "html.parser")
+
+
+
+
+    stress = soup.find("strong", {"class": "Cyrl headword"}).text
+    # meaning = soup.find("span", {"class": "mention-gloss"})
+
+    meaning = soup.find_all("span", {"class": "mention-gloss"})
+    meaning = "   ".join([a.text for a in meaning[:3]])
+
+
+
+    
+
+    if meaning:
+        pass
+    else:
+        meaning = soup.select('ol a')
+        meaning = "   ".join([a.text for a in meaning[:3]])
+
+
+
+        
+
+
+
+    
+
     
 else:
     print("span no")
 
-
-"""
-if i_tag:
-    print(i_tag.text)
-    print(soup)
-else:
-    print(1)
     html = requests.get("https://en.wiktionary.org/wiki/" + list)
     soup = BeautifulSoup(html.text, "html.parser")
-    print(soup)
-"""
+
+
+    stress = soup.find("strong", {"class": "Cyrl headword"}).text
+    meaning = soup.find_all("span", {"class": "mention-gloss"})
+    meaning = "   ".join([a.text for a in meaning[:3]])
+
+
+    
+    if meaning:
+        pass
+    else:
+        meaning = soup.select('ol a')
+        meaning = "   ".join([a.text for a in meaning[:3]])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(stress)
+print(meaning)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
