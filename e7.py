989218059bf0de74f63567c0e56163f945fc2af2
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2024-12-20 16:18:03 +0900

    Add files via upload

--- e7.py
+++ e7.py
@@ -16,119 +16,99 @@ warnings.filterwarnings('ignore', category=urllib3.exceptions.InsecureRequestWar
 
 
 
-list = "apple"
-print(list)
 
-html = requests.get("https://www.wordreference.com/enko/" + list)
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("it works very well really in paris.")
+
+lemmas = [token.lemma_ for token in doc if not token.is_stop]
+print(lemmas)
+
+
+
+
+print(doc[1].lemma_)
+print(doc[1].pos_)
+print(doc[1].is_stop)
+
+# print(nlp.Defaults.stop_words)
+
+print(88888888888888)
+
+
+
+
+
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+stop_words = stopwords.words('english')
+
+# print(stop_words)
+
+print(77777777777777777)
+
+
+
+
+
+
+doc = nlp("it shouldn't do very well in paris")
+
+# 불용어를 제외한 단어 추출
+filtered_tokens = [token.text for token in doc if not token.is_stop and token.text != '.']
+
+# 결과 출력
+print(filtered_tokens)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+word = "work"
+print(word)
+
+html = requests.get("https://www.wordreference.com/enko/" + word)
 soup = BeautifulSoup(html.text, "html.parser")
 
 span = soup.find("span", {"class": "pronWR tooltip pronWidget"})
-print(span.text.split("/")[1])
+pron = "[" + span.text.split("/")[1].replace("ˈ", "")+ "]"
+print(pron)
 
 
-soup = soup.find("tr", {"class": "even"})
-span = soup.find_all("td", {"class": "ToWrd"})
-print(span[0].text)
-
-
-
-if span:
-    print("span yes")
-    i_tag = soup.find("i", {"class": "Cyrl mention"})
-    print(i_tag.text)
-
-    html = requests.get("https://en.wiktionary.org/wiki/" + re.sub(r'\u0301', '', i_tag.text))
-    soup = BeautifulSoup(html.text, "html.parser")
-
-
-
-
-    stress = soup.find("strong", {"class": "Cyrl headword"})
-    if stress:
-        stress = stress.text
-    else:
-        stress = ""
-
-
-
-
-    meaning = soup.find_all("span", {"class": "mention-gloss"})
-    meaning = "   ".join([a.text for a in meaning[:3]])
-
-
-
-    
-
-    if meaning:
-        pass
-    else:
-        meaning = soup.select('ol a')
-        meaning = "   ".join([a.text for a in meaning[:3]])
-
-
-        if meaning:
-            pass
-        else:
-            meaning = ""
-
-
-
-        
-
-
-
-    
-
-    
-else:
-    print("span no")
-
-    html = requests.get("https://en.wiktionary.org/wiki/" + list)
-    soup = BeautifulSoup(html.text, "html.parser")
-
-
-    stress = soup.find("strong", {"class": "Cyrl headword"})
-    if stress:
-        stress = stress.text
-    else:
-        stress = ""
-
-
-
-
-
-    
-    meaning = soup.find_all("span", {"class": "mention-gloss"})
-    meaning = "   ".join([a.text for a in meaning[:3]])
-
-
-    
-    if meaning:
-        pass
-    else:
-        meaning = soup.select('ol a')
-        meaning = "   ".join([a.text for a in meaning[:3]])
-
-
-        if meaning:
-            pass
-        else:
-            meaning = ""
-
-
-
-
-
-
-
-
-
-
-
-
-
-print(stress)
+tr = soup.find_all("tr", {"class": "even"})
+meaning = ", ".join([tr_meaning.find("td", {"class": "ToWrd"}).contents[0].strip() for tr_meaning in tr[:5*3] if tr_meaning.find("td", {"class": "ToWrd"}) ])
 print(meaning)
+
+
+    
+print (word + " [" + pron + "] " + meaning)
 
 
 
